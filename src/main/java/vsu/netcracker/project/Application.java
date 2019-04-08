160c639cb26fab966ed04dc658566e7079d19f82
MISS-CEH4TOP <popowa.alinochka@gmail.com>
2019-04-08 17:25:18 +0300

    ...

---
+++ src/main/java/vsu/netcracker/project/Application.java
@@ -0,0 +1,19 @@
+package vsu.netcracker.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// ToDo - добавить в приложение логирование (по возможности и желанию) + еще можно тесты для кода
+// ToDo - повозиться с типами блюд в меню (у нас нет супов, но есть морепродукты и т. д.)
+
+/**
+ * Class with entry point of our app
+ *
+ * @author Кушнеренко Виктор
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
