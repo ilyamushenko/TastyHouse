a419a31d61e775e872ce920239504a69e8cbf7a3
Ilya Mushenko <ilmamen36@yandex.ru>
2019-04-10 12:22:12 +0300

    Добавил модель, которая хранит в себе и ингредиент и его необходимое на завтра количество

---
+++ src/main/java/vsu/netcracker/project/subModels/IngredientForTomorrow.java
@@ -0,0 +1,30 @@
+package vsu.netcracker.project.subModels;
+
+import vsu.netcracker.project.database.models.Ingredient;
+
+public class IngredientForTomorrow extends Ingredient {
+
+    private double quantityIngredientsForTomorrow;
+    private Ingredient ingredient;
+
+    public IngredientForTomorrow(Ingredient ingredient, double quantityIngredientsForTomorrow) {
+        this.quantityIngredientsForTomorrow = quantityIngredientsForTomorrow;
+        this.ingredient = ingredient;
+    }
+
+    public double getQuantityIngredientsForTomorrow() {
+        return quantityIngredientsForTomorrow;
+    }
+
+    public void setQuantityIngredientsForTomorrow(double quantityIngredientsForTomorrow) {
+        this.quantityIngredientsForTomorrow = quantityIngredientsForTomorrow;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+}
