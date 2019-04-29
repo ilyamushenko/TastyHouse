39180a73d79461f250904787a3c00a999d8c7d4a
Vitya535 <kushnerenko.viktor@gmail.com>
2019-04-29 21:02:50 +0300

    Попытался поставить время жизни токену.

--- src/main/java/vsu/netcracker/project/security/JwtGenerator.java
+++ src/main/java/vsu/netcracker/project/security/JwtGenerator.java
@@ -6,6 +6,9 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
 import vsu.netcracker.project.database.models.Staff;
 
+import java.util.Calendar;
+import java.util.Date;
+
 @Component
 public class JwtGenerator {
 
@@ -15,8 +18,13 @@ public class JwtGenerator {
         claims.put("id", String.valueOf(staff.getId()));
         claims.put("role", staff.getRoleStaff().getTitle());
         claims.put("password", staff.getPassword());
+        claims.put("token_create_date", new Date().getTime());
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MONTH, 2);
+        claims.put("token_expiration_date", calendar.getTime());
         return Jwts.builder()
                 .setClaims(claims)
+                .setExpiration(calendar.getTime())
                 .signWith(SignatureAlgorithm.HS256, "TastyHouse")
                 .compact();
     }
