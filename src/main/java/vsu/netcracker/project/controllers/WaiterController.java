cd1adafb892ea873c5a0eb5ea9117275c0300148
Andre <amm-andreistrizhko@yandex.ru>
2019-05-12 12:59:17 +0300

    shift user data to first argument

--- src/main/java/vsu/netcracker/project/controllers/WaiterController.java
+++ src/main/java/vsu/netcracker/project/controllers/WaiterController.java
@@ -105,6 +105,6 @@ public class WaiterController {
         }
         Map<Integer, List<?>> statusMap = Utils.convertListToMap(listOfOrderStatus, 1);
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return Arrays.asList(mapRestaurantTable, statusMap, principal);
+        return Arrays.asList(principal, mapRestaurantTable, statusMap);
     }
 }
